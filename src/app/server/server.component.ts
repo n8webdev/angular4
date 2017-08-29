01c6e5ec4d24fb52ebb11e8e8228281c238cfbb8
n8Torquato <n8webdev@gmail.com>
2017-08-29 20:38:03 -0300

    String interpolation options

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
+  serverID = 10;
+  serverStatus = 'offline';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getStatus(): string {
+    return this.serverStatus;
   }
 
 }
